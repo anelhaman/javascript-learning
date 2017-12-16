fae2df84e65b7c667f51ae24fcbdbeba912303d6
rachen <anelhaman@hotmail.com>
2017-12-16 13:49:09 +0700

    string and function string

--- script.js
+++
@@ -1,5 +0,0 @@
-{
-  document.write("Converted String  = ", Number("3.14"),"<br />")
-  document.write("Converted Int     = ", parseInt("5"),"<br />")
-  document.write("Converted Float   = ", parseFloat("3.1415"),"<br />")
-}
