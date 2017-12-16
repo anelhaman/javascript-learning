4465eacd2c7832dbdb02d6451ef01c849954cfbc
rachen <anelhaman@hotmail.com>
2017-12-16 12:37:02 +0700

    math and function math

---
+++ num.js
@@ -0,0 +1,60 @@
+{
+  document.write("5 + 4 = ",5+4,"<br />")
+  document.write("5 - 4 = ",5-4,"<br />")
+  document.write("5 * 4 = ",5*4,"<br />")
+  document.write("5 / 4 = ",5/4,"<br />")
+  document.write("5 % 4 = ",5%4,"<br />")
+}
+
+{
+  let balance = 1563.87
+  document.write("Monthy Payment : ",(balance/12).toFixed(2),"<br />")
+}
+{
+  let randNum = 5
+
+  document.write("randNum++ = ",randNum++,"<br />")
+  document.write("++randNum = ",++randNum,"<br />")
+}
+{
+  let randNum = 5
+  document.write("randNum-- = ",randNum--,"<br />")
+  document.write("--randNum = ",--randNum,"<br />")
+}
+{
+  let randNum = 5
+  document.write("randNum += 5 = ",randNum += 5,"<br />")
+  document.write("randNum -= 5 = ",randNum -= 5,"<br />")
+  document.write("randNum *= 5 = ",randNum *= 5,"<br />")
+  document.write("randNum /= 5 = ",randNum /= 5,"<br />")
+}
+{
+  let randNum = 5
+  document.write("3 + 2 * 5   = ",3 + 2 * 5,"<br />")
+  document.write("(3 + 2) * 5 = ",(3 + 2) * 5,"<br />")
+}
+{
+  document.write("Math.E  = ",Math.E ,"<br />")
+  document.write("Math.PI = ",Math.PI ,"<br />")
+}
+{
+  document.write("Math.abs(-8)      = ",Math.abs(-8),"<br />")
+  document.write("Math.cbrt(1000)   = ",Math.cbrt(1000),"<br />")
+  document.write("Math.ceil(6.45)   = ",Math.ceil(6.45),"<br />")
+  document.write("Math.floor(6.45)  = ",Math.floor(6.45),"<br />")
+  document.write("Math.round(6.45)  = ",Math.round(6.45),"<br />")
+  document.write("Math.log(10)      = ",Math.log(10),"<br />")
+  document.write("Math.log10(10)    = ",Math.log10(10),"<br />")
+  document.write("Math.max(10,5)    = ",Math.max(10,5),"<br />")
+  document.write("Math.min(10,5)    = ",Math.min(10,5),"<br />")
+  document.write("Math.pow(4,2)     = ",Math.pow(4,2),"<br />")
+  document.write("Math.sqrt(1000)   = ",Math.sqrt(1000),"<br />")
+}
+{
+  document.write("Random number (#1-10) = ",Math.floor(Math.random()*10)+1 ,"<br />")
+}
+{
+  document.write("Converted String  = ", Number("3.14"),"<br />")
+  document.write("Converted Int     = ", parseInt("5"),"<br />")
+  document.write("Converted Float   = ", parseFloat("3.1415"),"<br />")
+}
