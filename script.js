ee6878dd65474e6f2a291dccdb6551db6661deab
rachen <anelhaman@hotmail.com>
2017-12-16 12:23:55 +0700

    math and function

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
-let yourName = prompt("What is your name")
-
-if(yourName != null){
-  document.getElementById("sayHello").innerHTML = "Hello, "+yourName;
+{
+  document.write("5 + 4 = ",5+4,"<br />")
+  document.write("5 - 4 = ",5-4,"<br />")
+  document.write("5 * 4 = ",5*4,"<br />")
+  document.write("5 / 4 = ",5/4,"<br />")
+  document.write("5 % 4 = ",5%4,"<br />")
 }
-else {
-  alert("Please enter your name next time.")
+
+{
+  let balance = 1563.87
+  document.write("Monthy Payment : ",(balance/12).toFixed(2),"<br />")
+}
+{
+  let randNum = 5
+
+  document.write("randNum++ = ",randNum++,"<br />")
+  document.write("++randNum = ",++randNum,"<br />")
+}
+{
+  let randNum = 5
+  document.write("randNum-- = ",randNum--,"<br />")
+  document.write("--randNum = ",--randNum,"<br />")
+}
+{
+  let randNum = 5
+  document.write("randNum += 5 = ",randNum += 5,"<br />")
+  document.write("randNum -= 5 = ",randNum -= 5,"<br />")
+  document.write("randNum *= 5 = ",randNum *= 5,"<br />")
+  document.write("randNum /= 5 = ",randNum /= 5,"<br />")
+}
+{
+  let randNum = 5
+  document.write("3 + 2 * 5   = ",3 + 2 * 5,"<br />")
+  document.write("(3 + 2) * 5 = ",(3 + 2) * 5,"<br />")
+}
+{
+  document.write("Math.E  = ",Math.E ,"<br />")
+  document.write("Math.PI = ",Math.PI ,"<br />")
+}
+{
+  document.write("Math.abs(-8)      = ",Math.abs(-8),"<br />")
+  document.write("Math.cbrt(1000)   = ",Math.cbrt(1000),"<br />")
+  document.write("Math.ceil(6.45)   = ",Math.ceil(6.45),"<br />")
+  document.write("Math.floor(6.45)  = ",Math.floor(6.45),"<br />")
+  document.write("Math.round(6.45)  = ",Math.round(6.45),"<br />")
+  document.write("Math.log(10)      = ",Math.log(10),"<br />")
+  document.write("Math.log10(10)    = ",Math.log10(10),"<br />")
+  document.write("Math.max(10,5)    = ",Math.max(10,5),"<br />")
+  document.write("Math.min(10,5)    = ",Math.min(10,5),"<br />")
+  document.write("Math.pow(4,2)     = ",Math.pow(4,2),"<br />")
+  document.write("Math.sqrt(1000)   = ",Math.sqrt(1000),"<br />")
+}
+{
+  document.write("Random number (#1-10) = ",Math.floor(Math.random()*10)+1 ,"<br />")
 }
